263650c9a9cf6f63e8e0116954aaec282d1f5fec
Luis-Guilherme-Molotto <67129787+Luis-Guilherme-Molotto@users.noreply.github.com>
2022-06-09 15:46:56 -0300

    Add files via upload

---
+++ gerador 1.py
@@ -0,0 +1,26 @@
+import math
+
+tam = int(60000000)
+a = int(16807)
+m = int(math.pow(2, 31) - 1)
+i = int(1)
+
+n = []
+r = []
+
+n.append(2022) #primeiro elemento do array n[0] = 2022
+r.append("")
+
+while (i < tam):
+  print(i)
+  
+  valor = ( (a * n[i-1]) % m )
+  n.append(valor)
+  
+  valor = valor / m
+  r.append(valor)
+
+  i = i + 1
+
+print(n)
+print(r)
