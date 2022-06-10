2b0129ad5348d770f50bf74438e0693218903c96
hironha <rafaelhirokato@gmail.com>
2022-06-09 21:23:59 -0300

    :recycle: troca dos parametros do gerador 1

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from typing import List
 from timeit import timeit
 
 from gerador_aleatorio import GeradorAleatorio
 
 
-def gerar_numeros_aleatorios() -> List[int]:
+def gerar_numeros_aleatorios():
     gerador = GeradorAleatorio(tamanho=6 * (10 ** 7))
 
     numeros_aleatorios = gerador.exec()
@@ -23,7 +22,7 @@ def testar_geracao() -> None:
 
 
 def main():
-    gerador_1 = GeradorAleatorio(tamanho=10)
+    gerador_1 = GeradorAleatorio(multiplicador=16807, modulo=((2 ** 31) - 1))
     print(gerador_1.exec())
 
     # testar performance
