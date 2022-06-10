e4b2292fa87ecfe7598f939e78c9733ead192604
hironha <rafaelhirokato@gmail.com>
2022-06-10 20:27:37 -0300

    :sparkles: add codigo para gerar arquivo com numeros aleatorios

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from timeit import timeit
+from os.path import exists as verificar_existe_arquivo
+from typing import List
 
 from gerador_aleatorio import GeradorAleatorio
 
 
 def gerar_numeros_aleatorios():
-    gerador = GeradorAleatorio(tamanho=6 * (10 ** 7))
+    gerador = GeradorAleatorio()
 
-    numeros_aleatorios = gerador.exec()
-
-    return numeros_aleatorios
+    return gerador.exec(tamanho=6 * (10 ** 7))
 
 
-def testar_geracao() -> None:
+def cronometrar_tempo() -> None:
     tempo = timeit(
         "gerar_numeros_aleatorios()",
         setup="from __main__ import gerar_numeros_aleatorios",
@@ -21,12 +21,41 @@ def testar_geracao() -> None:
     print("Tempo de execucao: " + str(tempo))
 
 
-def main():
-    gerador_1 = GeradorAleatorio(multiplicador=16807, modulo=((2 ** 31) - 1))
-    print(gerador_1.exec())
+def escrever_arquivo(nome: str, text: str):
+    try:
+        existe_arquivo = verificar_existe_arquivo(nome)
+        arquivo = open(nome, "w" if existe_arquivo else "x")
+        arquivo.write(text)
+        arquivo.close()
+    except:
+        print("Nao foi possivel escrever no arquivo")
 
+
+def stringify_numeros(vetor: List[float], separador: str):
+    string = "".join(["%.12f" % numero + separador for numero in vetor])
+
+    return string
+
+
+def main():
     # testar performance
-    # testar_geracao()
+    # cronometrar_tempo()
+
+    # gerador 1
+    # gerador_1 = GeradorAleatorio(multiplicador=16807, modulo=((2 ** 31) - 1))
+
+    # gerador personalizado
+    gerador_personalizado = GeradorAleatorio(
+        multiplicador=8404997, modulo=((2 ** 35) - 1)
+    )
+
+    numeros_aleatorios = gerador_personalizado.exec(tamanho=6 * (10 ** 7))
+
+    print("transformando numeros em string...")
+    texto_arquivo = stringify_numeros(numeros_aleatorios, separador="\n")
+
+    print("escrevendo no arquivo...")
+    escrever_arquivo("GERALEO.txt", texto_arquivo)
 
 
 if __name__ == "__main__":
--- gerador_aleatorio.py
+++ gerador_aleatorio.py
@@ -4,13 +4,11 @@ from math import pow as Pow
 class GeradorAleatorio:
     def __init__(
         self,
-        tamanho: int = 20,
         semente: int = 1000,
         multiplicador: int = 8404997,
         incremento: int = 1,
         modulo: int = Pow(2, 35),
     ) -> None:
-        self._tamanho = tamanho
         self._semente = semente
         self._multiplicador = multiplicador
         self._incremento = incremento
@@ -23,10 +21,10 @@ class GeradorAleatorio:
 
         return int(num_aleatorio)
 
-    def exec(self):
+    def exec(self, tamanho: int = 20):
         numero_anterior = self._semente
         valores_aleatorios: list[float] = []
-        for i in range(self._tamanho - 1):
+        for i in range(tamanho - 1):
             numero_anterior = self.__calc_numero_aleatorio(numero_anterior)
             valores_aleatorios.append(numero_anterior / self._modulo)
 
