cd17e61ae2247745457b3911524f2af8eb6a1628
hironha <rafaelhirokato@gmail.com>
2022-06-12 11:25:59 -0300

    :recycle: add métodos compartilhados em classe abstrata

--- teste_ks.py
+++ teste_ks.py
@@ -1,20 +1,40 @@
 from abc import ABC, abstractmethod
+from math import sqrt
 from typing import List
 
 
 class TesteKS(ABC):
+    def __init__(self, precisao: int = 12) -> None:
+        self._precisao = precisao
+
     @abstractmethod
     def _calc_fo(self, li: List[float], ls: List[float]) -> List[float]:
         pass
 
-    @abstractmethod
+    def _soma_acumulada(self, lista: List[float] | List[int]):
+        soma_acc = [lista[0]]
+
+        for i in range(len(lista) - 1):
+            soma_acc.append(round((soma_acc[i] + lista[i + 1]), self._precisao))
+
+        return soma_acc
+
     def _calc_pi(self, fo: List[float]) -> List[float]:
-        pass
+        fo_soma = sum(fo)
 
-    @abstractmethod
+        return [freq_observ / fo_soma for freq_observ in fo]
+
     def _calc_gx(self, pi: List[float]) -> List[float]:
-        pass
+        return self._soma_acumulada(pi)
 
-    @abstractmethod
     def _calc_teste(self, fx: List[float], gx: List[float]) -> List[float]:
-        pass
+        if len(fx) != len(gx):
+            raise Exception("parametros de _calc_teste invalidos")
+
+        return [abs(round((fx[i] - gx[i]), self._precisao)) for i in range(len(fx))]
+
+    def _calc_ks(self, teste: List[float]) -> float:
+        return max(teste)
+
+    def _calc_ks5(self, numeros: List[float]) -> float:
+        return 1.36 / sqrt(len(numeros))
--- teste_uniformidade.py
+++ teste_uniformidade.py
@@ -9,7 +9,7 @@ from teste_ks import TesteKS
 class TesteUniformidade(TesteKS):
     def __init__(self, classes: int = 20, precisao: int = 12) -> None:
         self._classes = classes
-        self._precisao = precisao
+        super().__init__(precisao=precisao)
 
     def _calc_li(self):
         return [i / self._classes for i in range(self._classes)]
@@ -26,34 +26,17 @@ class TesteUniformidade(TesteKS):
                     fo[index] += 1
         return fo
 
-    def _calc_pi(self, numeros: List[float], fo: List[float]):
-        len_numeros = len(numeros)
-        return [fo[i] / len_numeros for i in range(self._classes)]
-
-    def _calc_gx(self, pi: List[float]):
-        gx = [pi[0]]
-
-        for i in range(len(pi) - 1):
-            gx.append(round((gx[i] + pi[i + 1]), self._precisao))
-
-        return gx
-
-    def _calc_teste(self, fx: List[float], gx: List[float]):
-        return [
-            abs(round((fx[i] - gx[i]), self._precisao)) for i in range(self._classes)
-        ]
-
     def exec(self, numeros: List[float]):
         li = self._calc_li()
         ls = self._calc_ls()
         fo = self._calc_fo(numeros, li, ls)
-        pi = self._calc_pi(numeros, fo)
+        pi = self._calc_pi(fo)
         gx = self._calc_gx(pi)
         fx = ls
 
         teste = self._calc_teste(fx, gx)
-        ks = max(teste)
-        ks5 = 1.36 / sqrt(len(numeros))
+        ks = self._calc_ks(teste)
+        ks5 = self._calc_ks5(numeros)
 
         print("Aceita H0" if ks < ks5 else "Rejeita H0")
         print(ks)
